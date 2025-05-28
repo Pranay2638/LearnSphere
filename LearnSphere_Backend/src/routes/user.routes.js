ece33371badafdf27b86712f7181680dfb6c07c3
Pranay2638 <pranaysanjayv@gmail.com>
2025-05-28 15:13:07 +0530

    fix: Rename router variable to userRouter for clarity and consistency

--- LearnSphere_Backend/src/routes/user.routes.js
+++ LearnSphere_Backend/src/routes/user.routes.js
@@ -2,22 +2,22 @@ import {Router} from 'express';
 import { registerUser, loginUser, refreshAccessToken, getUserProfile, logoutUser } from '../controllers/user.controller.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
 
-const router = Router();
+const userRouter = Router();
 
-router.route('/register').post(registerUser)
+userRouter.route('/register').post(registerUser)
 
-router.route('/login').post(loginUser)
+userRouter.route('/login').post(loginUser)
 
-router.route('/logout').post(authMiddleware, logoutUser)
+userRouter.route('/logout').post(authMiddleware, logoutUser)
 
-router.route('/refresh-token').post(refreshAccessToken)
+userRouter.route('/refresh-token').post(refreshAccessToken)
 
-router.route('/current-user').get(authMiddleware, getUserProfile)
+userRouter.route('/current-user').get(authMiddleware, getUserProfile)
 
-router.get('/dashboard', authMiddleware, (req, res) => {
+userRouter.get('/dashboard', authMiddleware, (req, res) => {
     res.json({message: `welcome, user ${req.user.id}`})
 })
 
 
 
-export default router;
+export default userRouter;
